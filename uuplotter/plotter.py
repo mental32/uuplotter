76ab1317380db8d21f8d5dcd1f3cd85bf3a61c33
mental32 <m3nta1@yahoo.com>
2018-02-06 02:19:11 +0000

    Added extra methods to uuploter.plotter.Box

--- uuplotter/plotter.py
+++ uuplotter/plotter.py
@@ -9,16 +9,33 @@ class Box:
 	def estimate(cls, hostname) -> int:
 		return len(hostname) + 4
 
-	def __init__(self, name, connections=[]):
+	def __init__(self, name, connections=[], sorted_connections=True):
 		self.raw_str = ['╔{0}╗', '{1} {2} {3}', '╚{4}╝']
-		self.connections = sorted(connections)
+		self.connections = (sorted(connections) if sorted_connections else connections)
 		self.name = name
 
-		self.left_connector = self.right_connector = '║'
-		self.top_connector = self.bottom_connector = '═' * (len(self.name) + 2)
+		self.left_connector = '║'
+		self.right_connector = '║'
+		
+		self.top_connector = '═' * (len(self.name) + 2)
+		self.bottom_connector = '═' * (len(self.name) + 2)
 
 	def __repr__(self):
 		return 'Box(name={0}, connections={1})'.format(self.name, len(self.connections))
+
+	def pad_left(self, item):
+		for i, c in enumerate(self.raw_str.copy()):
+			self.raw_str[i] = item + c
+
+	def attach(self, side, seq):
+		if side == 'left':
+			for s, rs in zip(seq, enumerate(self.raw_str.copy())):
+				self.raw_str[rs[0]] = s + self.raw_str[rs[0]]
+		elif side == 'right':
+			for s, rs in zip(seq, enumerate(self.raw_str.copy())):
+				self.raw_str[rs[0]] = self.raw_str[rs[0]] + s
+		else:
+			raise Exception
 
 	def modify(self, **kwargs):
 		for key, value in kwargs.items():
